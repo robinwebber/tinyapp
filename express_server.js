710a0152049cac3ff6b2d66993a97de81d812324
robinwebber <robinewebber@gmail.com>
2019-07-09 17:10:20 +0000

    Added start nodemon and npm start functionality

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const generateRandomString = () => {
-  return Math.random().toString(36).substring(2, 8)
+  return Math.random().toString(36).substring(2, 8);
 };
 
 const urlDatabase = {
@@ -35,16 +35,21 @@ app.get('/urls/new', (req, res) => {
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
-})
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.post("/urls", (req, res) => {
   let shortURLCode = generateRandomString();
   urlDatabase[shortURLCode] = req.body.longURL;
-  res.redirect(`/urls/${shortURLCode}`);         
+  res.redirect(`/urls/${shortURLCode}`);
 });
 
 
