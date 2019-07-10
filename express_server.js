13cdfa477179eb189bf91243cf1741e91c3675f1
robinwebber <robinewebber@gmail.com>
2019-07-10 22:17:34 +0000

    user registration functioning

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 var cookieParser = require('cookie-parser');
 app.use(cookieParser());
 const uuidv4 = require('uuid/v4');
@@ -13,30 +13,30 @@ const updateLongURL = (shortURL, longURL) => {
   urlDatabase[shortURL] = longURL;
 };
 
-const users = { 
+const users = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
- "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 const userCreater = (id, email, password) => {
-  users[id] = {id, email, password};
+  users[id] = { id, email, password };
 };
 
-const emailDuplicateChecker = (email) => {
+const validator = (email) => {
   for (keys in users) {
     if (users[keys].email === email) {
-      return true;
+      return { valid: true, user: users[keys] }
     }
   }
-  return false;
+  return { valid: false }
 };
 
 const urlDatabase = {
@@ -64,7 +64,7 @@ app.get('/urls/new', (req, res) => {
   let templateVars = { user: users[req.cookies.user_id] };
   //console.log(req.cookies);
   res.render('urls_new', templateVars);
-  
+
 });
 
 app.get('/urls/:shortURL', (req, res) => {
@@ -84,34 +84,46 @@ app.get("/u/:shortURL", (req, res) => {
 app.post("/register", (req, res) => {
   if (req.body.email.length === 0 || req.body.password.length === 0) {
     res.status(400).send('Error: email or password left blank.');
-    
-  } else if (emailDuplicateChecker(req.body.email)) {
+
+  } else if (validator(req.body.email).valid) {
     res.status(400).send('Error: email already registered.')
   } else {
-    id = uuidv4().slice(0,6);
-  userCreater(id, req.body.email, req.body.password);
-  res.cookie('user_id', id);
-  res.redirect("/urls")
+    id = uuidv4().slice(0, 6);
+    userCreater(id, req.body.email, req.body.password);
+    res.cookie('user_id', id);
+    res.redirect("/urls")
   }
 });
 
 app.get("/register", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
-  //console.log('line 60 -->', req.body.email);
-  //console.log()
+
   res.render('registration', templateVars);
+});
+
+app.get("/login", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
+
+  res.render('login', templateVars);
 })
 
 app.post("/login", (req, res) => {
-  //console.log('line 56 -->', req.body.username);
-  res.cookie('username', req.body.username)
+
+  const email = req.body.email;
+  const validatedUser = validator(email);
+  if (!validatedUser.valid) {
+    res.status(403).send('Error: Log in failed.');
+
+  } else if (validatedUser.user.password === req.body.password) {
+    res.cookie('user_id', validator(email).user.id);
+  }
 
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
 
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect("/urls")
 })
 
@@ -140,7 +152,7 @@ app.post('/urls/:id/update', (req, res) => {
 })
 
 app.post("/urls", (req, res) => {
-  let shortURLCode = uuidv4().slice(0,6);
+  let shortURLCode = uuidv4().slice(0, 6);
   urlDatabase[shortURLCode] = req.body.longURL;
   res.redirect(`/urls/${shortURLCode}`);
 });
